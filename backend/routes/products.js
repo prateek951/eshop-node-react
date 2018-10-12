2fd25da0c968dc8bb97a1167c39abc45b4e433e5
prateek951 <prince.cenation@gmail.com>
2018-10-12 10:00:20 +0530

    adding an index

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,8 +1,8 @@
 const Router = require("express").Router;
 const mongodb = require("mongodb");
-const HTTP_STATUS_CODES = require('http-status-codes');
+const HTTP_STATUS_CODES = require("http-status-codes");
 // For storing images we use Decimal128
-const db = require('../db');
+const db = require("../db");
 const { Decimal128, ObjectId } = mongodb;
 const router = Router();
 
@@ -59,8 +59,8 @@ const router = Router();
 router.get("/", (req, res, next) => {
   // Return a list of dummy products
   // Later, this data will be fetched from MongoDB
-    const queryPage = req.query.page;
-    const pageSize = 1;
+  const queryPage = req.query.page;
+  const pageSize = 1;
   // let resultProducts = [...products];
   // if (queryPage) {
   //   resultProducts = products.slice(
@@ -68,45 +68,58 @@ router.get("/", (req, res, next) => {
   //     queryPage * pageSize
   //   );
   // }
+
+  //for changing the configuration or adding the index 
+  //we will do this outside the application 
+  // db.products.createIndex({price: -1})
+  
   const products = [];
-  db.GET_DATABASE().db().collection("products")
-        .find({}).sort({price:-1}).skip((queryPage-1)*pageSize)
-        .limit(pageSize).forEach(product => {
-          // console.log(product);
-          product.price = product.price.toString();
-          products.push(product);
-        })
-        .then(result => {
-          console.log(result);
-          res.status(HTTP_STATUS_CODES.OK).json(products);
-        })
-        .catch(err => {
-          console.log(err);
-          res
-            .status(HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR)
-            .json({ message: "An error occured" });
-        });
+  db.GET_DATABASE()
+    .db()
+    .collection("products")
+    .find({})
+    .sort({ price: -1 })
+    .skip((queryPage - 1) * pageSize)
+    .limit(pageSize)
+    .forEach(product => {
+      // console.log(product);
+      product.price = product.price.toString();
+      products.push(product);
     })
+    .then(result => {
+      console.log(result);
+      res.status(HTTP_STATUS_CODES.OK).json(products);
+    })
+    .catch(err => {
+      console.log(err);
+      res
+        .status(HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR)
+        .json({ message: "An error occured" });
+    });
+});
 
 // Get single product
-router.get('/:id',(req, res, next) => {
-    // const product = products.find(p => p._id === req.params.id);
-    // res.json(product);
-    const { id } = req.params;
-    db.GET_DATABASE().db().collection('products')
-    .findOne({_id : new ObjectId(id) })
+router.get("/:id", (req, res, next) => {
+  // const product = products.find(p => p._id === req.params.id);
+  // res.json(product);
+  const { id } = req.params;
+  db.GET_DATABASE()
+    .db()
+    .collection("products")
+    .findOne({ _id: new ObjectId(id) })
     .then(product => {
       product.price = product.price.toString();
-      if(product) {
+      if (product) {
         res.status(HTTP_STATUS_CODES.OK).json(product);
       }
     })
     .catch(err => {
-        console.log(err);
-        res.status(HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR).json({message:'An error occured'});
+      console.log(err);
+      res
+        .status(HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR)
+        .json({ message: "An error occured" });
     });
 });
-
 
 // Add new product
 // Requires logged in user
@@ -117,22 +130,23 @@ router.post("", (req, res, next) => {
     price: Decimal128.fromString(req.body.price.toString()), // store this as 128bit decimal in MongoDB
     image: req.body.image
   };
-  db.GET_DATABASE().db()
-        .collection("products")
-        .insertOne(newProduct)
-        .then(result => {
-          console.log(result);
-          res
-            .status(HTTP_STATUS_CODES.OK)
-            .json({ message: "Product added", productId: result.insertedId });
-        })
-        .catch(err => {
-          console.log(err);
-          res
-            .status(HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR)
-            .json({ message: "An error occured", productId: "DUMMY" });
-        });
+  db.GET_DATABASE()
+    .db()
+    .collection("products")
+    .insertOne(newProduct)
+    .then(result => {
+      console.log(result);
+      res
+        .status(HTTP_STATUS_CODES.OK)
+        .json({ message: "Product added", productId: result.insertedId });
+    })
+    .catch(err => {
+      console.log(err);
+      res
+        .status(HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR)
+        .json({ message: "An error occured", productId: "DUMMY" });
     });
+});
 // Edit existing product
 // Requires logged in user
 router.patch("/:id", (req, res, next) => {
@@ -143,27 +157,40 @@ router.patch("/:id", (req, res, next) => {
     image: req.body.image
   };
   const { id } = req.params;
-  db.GET_DATABASE().db().collection('products')
-  .updateOne({_id : new ObjectId(id)},{$set: updatedProduct}).then(() => {
-    res.status(HTTP_STATUS_CODES.CREATED).json({ message: "Product updated", productId: id });
-  }).catch(err => {
-    console.log(err);
-    res.status(HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR).json({ message: 'An error occured'});
-  })
+  db.GET_DATABASE()
+    .db()
+    .collection("products")
+    .updateOne({ _id: new ObjectId(id) }, { $set: updatedProduct })
+    .then(() => {
+      res
+        .status(HTTP_STATUS_CODES.CREATED)
+        .json({ message: "Product updated", productId: id });
+    })
+    .catch(err => {
+      console.log(err);
+      res
+        .status(HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR)
+        .json({ message: "An error occured" });
+    });
 });
 
 // Delete a product
 // Requires logged in user
-router.delete('/:id',(req, res, next) => {
+router.delete("/:id", (req, res, next) => {
   const { id } = req.params;
-  db.GET_DATABASE().db().collection('products')
-  .deleteOne({_id : new ObjectId(id)})
-  .then(() => {
-    res.status(HTTP_STATUS_CODES.OK).json({message:'Product deleted'});
-  }).catch(err => {
-    console.log(err);
-    res.status(HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR).json({message: 'An error occured'});
-  });
+  db.GET_DATABASE()
+    .db()
+    .collection("products")
+    .deleteOne({ _id: new ObjectId(id) })
+    .then(() => {
+      res.status(HTTP_STATUS_CODES.OK).json({ message: "Product deleted" });
+    })
+    .catch(err => {
+      console.log(err);
+      res
+        .status(HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR)
+        .json({ message: "An error occured" });
+    });
 });
 
 module.exports = router;
